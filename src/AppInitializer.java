31c92ffa7ea6e4ad120b9ea845744fa360727264
oshadaera68 <mindartlk010@gmail.com>
2021-12-14 11:02:51 +0530

    get method

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -16,9 +16,17 @@ public class AppInitializer {
         customer.setAddress("galle");
         customer.setSalary(34500);
 
+        Customer c2 = new Customer();
+        c2.setId("S002");
+        c2.setName("amesh");
+        c2.setAddress("galle");
+        c2.setSalary(34562);
+
         Session s1 = FactoryConfig.getInstance().getSession();
         Transaction transaction = s1.beginTransaction();
-        s1.save(customer);
+//        s1.delete(c2);
+        Customer s001 = s1.get(Customer.class, "S001");
+        System.out.println(s001.getId());
         transaction.commit();
         s1.close();
     }
